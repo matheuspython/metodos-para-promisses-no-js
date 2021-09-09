6a13cad2c661894e0f053c875fa8bc6a04e9f522
matheus souza <ruivorobado@gmail.com>
2021-09-09 09:50:14 -0300

    metodo Promisse,all()

--- index.js
+++ index.js
@@ -9,7 +9,44 @@ function espera(mensagem, tempo){
     if(typeof mensagem !== 'string') reject(false);
 
     setTimeout(()=> {
-      resolve(mensagem)
+      resolve(mensagem + ' promisse aceita')
     }, tempo)
   })
-}
+}//promisse que espera uma mensagem do tipo string
+
+/*
+  metodos
+    Promise.all
+    Promise.race
+    Promise.reject
+*/
+
+/*
+  promisse all
+
+  vc passa um array com promisses ou valores ja resolvidos
+  e ele vai te retornar uma promessa com os valores dentro de um array
+*/
+const promises = [
+  'valor 1',
+  espera('Promisse 1', 3000),
+  espera('Promisse 2', 500),
+  espera('Promisse 3', 1000),
+  'outro valor'
+];
+
+Promise.all(promises).then(function(valor){
+  console.log(valor)
+}).catch(function(error){
+  console.log(error)
+})
+
+/* retorno
+  [
+   'valor 1',
+   'Promisse 1',
+   'Promisse 2',
+   'Promisse 3',
+   'outro valor'
+  ]
+*/
